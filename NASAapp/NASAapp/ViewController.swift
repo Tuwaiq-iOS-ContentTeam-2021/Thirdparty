48709345edf2dbc89d9bb886a55c6e063e95db57
taghreed2 <taghreed.mohammed.ruh.s@tuwaiq.edu.sa>
2021-12-05 22:50:03 +0300

    Taghreed Alotaibi

---
+++ NASAapp/NASAapp/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  NASAapp
+//
+//  Created by TAGHREED on 01/05/1443 AH.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageLable: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    let apiURL = "https://api.nasa.gov/planetary/apod?api_key="
+    let apiKey = "hDGkFGblYnvqaGsBDdGJi8CdTVrXMJmTDwffdaLf"
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let datee = Date()
+        datePicker.maximumDate = Date?(datee)
+        datePickerHandler((Any).self)
+        datePicker.layer.cornerRadius = 20
+        
+    }
+    
+    @IBAction func datePickerHandler(_ sender: Any) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let selectedDate = dateFormatter.string(from: datePicker.date)
+        
+        requestImage(date: selectedDate)
+        
+    }
+    
+    
+    func requestImage (date: String){
+        
+        let parameters : [String:String] = ["date":date]
+        AF.request(apiURL + apiKey, method: .get, parameters: parameters)
+            .responseJSON {
+                response in
+                
+                switch response.result {
+                    
+                case .success(let value):
+                    print("JSON:\(value)")
+                    
+                    guard let response = value as? [String: String] else {
+                        print("Error..")
+                        return
+                    }
+                    
+                    let imageTitle = response ["title"]
+                    self.imageLable.text = imageTitle
+                    
+                    let image = response ["url"]
+                    let imageURL = URL(string: image!)
+                    let imageData = try! Data(contentsOf: imageURL!)
+                    
+                    
+                    self.img.image = UIImage(data: imageData)
+                    
+                    
+                    
+                case .failure(let error):
+                    print("error:\(error)")
+                    
+                }
+            }
+    }
+    
+}
+
